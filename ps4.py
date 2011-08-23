89fbe3e2565ac3b2190351b165c9389758100796
linjunhalida <linjunhalida@gmail.com>
2011-08-23 20:33:29 +0800

    ps4

---
+++ ps4.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+"""
+module: ps4
+"""
+# Problem Set 4
+# Name: halida
+# Collaborators: None
+# Time: 00:28
+
+#
+# Problem 1
+#
+
+def nestEggFixed(salary, save, growthRate, years):
+    """
+    - salary: the amount of money you make each year.
+    - save: the percent of your salary to save in the investment account each
+      year (an integer between 0 and 100).
+    - growthRate: the annual percent increase in your investment account (an
+      integer between 0 and 100).
+    - years: the number of years to work.
+    - return: a list whose values are the size of your retirement account at
+      the end of each year.
+    """
+    ps = []
+    for i in range(years):
+        p = ps[i-1] if i > 0 else 0
+        ps.append(  p * (1 + 0.01 * growthRate) + salary * save * 0.01 )
+    return ps
+
+def testNestEggFixed():
+    salary     = 10000
+    save       = 10
+    growthRate = 15
+    years      = 5
+    savingsRecord = nestEggFixed(salary, save, growthRate, years)
+    print savingsRecord
+    # Output should have values close to:
+    # [1000.0, 2150.0, 3472.5, 4993.375, 6742.3812499999995]
+
+    # TODO: Add more test cases here.
+
+#
+# Problem 2
+#
+
+def nestEggVariable(salary, save, growthRates):
+    """
+    - salary: the amount of money you make each year.
+    - save: the percent of your salary to save in the investment account each
+      year (an integer between 0 and 100).
+    - growthRate: a list of the annual percent increases in your investment
+      account (integers between 0 and 100).
+    - return: a list of your retirement account value at the end of each year.
+    """
+    ps = []
+    for i, growthRate in enumerate(growthRates):
+        p = ps[i-1] if i > 0 else 0
+        ps.append(  p * (1 + 0.01 * growthRate) + salary * save * 0.01 )
+    return ps
+
+def testNestEggVariable():
+    salary      = 10000
+    save        = 10
+    growthRates = [3, 4, 5, 0, 3]
+    savingsRecord = nestEggVariable(salary, save, growthRates)
+    print savingsRecord
+    # Output should have values close to:
+    # [1000.0, 2040.0, 3142.0, 4142.0, 5266.2600000000002]
+
+    # TODO: Add more test cases here.
+
+#
+# Problem 3
+#
+
+def postRetirement(savings, growthRates, expenses):
+    """
+    - savings: the initial amount of money in your savings account.
+    - growthRate: a list of the annual percent increases in your investment
+      account (an integer between 0 and 100).
+    - expenses: the amount of money you plan to spend each year during
+      retirement.
+    - return: a list of your retirement account value at the end of each year.
+    """
+    ps = []
+    for i, growthRate in enumerate(growthRates):
+        f = ps[i-1] if i > 0 else savings
+        ps.append(  f * (1 + 0.01 * growthRate) - expenses )
+    return ps
+    
+
+def testPostRetirement():
+    savings     = 100000
+    growthRates = [10, 5, 0, 5, 1]
+    expenses    = 30000
+    savingsRecord = postRetirement(savings, growthRates, expenses)
+    print savingsRecord
+    # Output should have values close to:
+    # [80000.000000000015, 54000.000000000015, 24000.000000000015,
+    # -4799.9999999999854, -34847.999999999985]
+
+    # TODO: Add more test cases here.
+
+#
+# Problem 4
+#
+
+def findMaxExpenses(salary, save, preRetireGrowthRates, postRetireGrowthRates,
+                    epsilon):
+    """
+    - salary: the amount of money you make each year.
+    - save: the percent of your salary to save in the investment account each
+      year (an integer between 0 and 100).
+    - preRetireGrowthRates: a list of annual growth percentages on investments
+      while you are still working.
+    - postRetireGrowthRates: a list of annual growth percentages on investments
+      while you are retired.
+    - epsilon: an upper bound on the absolute value of the amount remaining in
+      the investment fund at the end of retirement.
+    """
+    savings = nestEggVariable(salary, save, preRetireGrowthRates)[-1]
+    # print "saving is : %f" % savings
+    explow = 0
+    exphigh = savings + epsilon
+    exp = (explow + exphigh) / 2.0
+    while True:
+        remains = postRetirement(savings, postRetireGrowthRates, exp)[-1]
+        # print "with exp %f, remains %f" % (exp, remains)
+        if remains > epsilon:
+            explow = exp
+            exp = (explow + exphigh) / 2.0
+        elif remains < -epsilon:
+            exphigh = exp
+            exp = (explow + exphigh) / 2.0
+        else:
+            break
+    return exp
+
+def testFindMaxExpenses():
+    salary                = 10000
+    save                  = 10
+    preRetireGrowthRates  = [3, 4, 5, 0, 3]
+    postRetireGrowthRates = [10, 5, 0, 5, 1]
+    epsilon               = .01
+    expenses = findMaxExpenses(salary, save, preRetireGrowthRates,
+                               postRetireGrowthRates, epsilon)
+    print expenses
+    # Output should have a value close to:
+    # 1229.95548986
+
+    # TODO: Add more test cases here.
+
+def main():
+    """
+    >>> testNestEggFixed()
+    [1000.0, 2150.0, 3472.5, 4993.375, 6742.3812499999995]
+    >>> nestEggFixed(60000, 50, 3, 20)[-1]
+    806111.23466941412
+
+    >>> testNestEggVariable()
+    [1000.0, 2040.0, 3142.0, 4142.0, 5266.2600000000002]
+
+    >>> testPostRetirement()
+    [80000.000000000015, 54000.000000000015, 24000.000000000015, -4799.9999999999854, -34847.999999999985]
+
+    >>> testFindMaxExpenses()
+    1229.9528031
+    """
+    import doctest
+    doctest.testmod()
+    
+if __name__=="__main__":
+    main()
